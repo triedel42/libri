510566c20cb55b372f4d7ed3c7197fe406ed98f2
Tobias Riedel <193485289+TEQ-Tobias@users.noreply.github.com>
2026-05-07 20:22:29 +0200

    fix(infra): consistent ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    port: 8000,
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://localhost:5173',
     },
     allowedHosts: process.env.HOST ? [process.env.HOST] : [],
   },
